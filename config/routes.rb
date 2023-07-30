a19f7bae1cfd510d3aa67f8b3addc7a552e91e75
Manoj Mishra <manojmishra.deb@gmail.com>
2023-07-30 13:02:50 +0530

    Add line item dates on quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: %i[ index show ]
+  end
 
   root to: 'pages#home'
 end
---
+++ test/controllers/line_item_dates_controller_test.rb
@@ -0,0 +1,4 @@
+require "test_helper"
+
+class LineItemDatesControllerTest < ActionDispatch::IntegrationTest
+end
